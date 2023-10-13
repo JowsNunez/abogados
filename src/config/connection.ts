0bd00277d4f0a45faa4f6fbee5553c01e271dcaf
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-10-13 11:54:38 -0700

    chore: configuracion .env bd

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,10 +1,17 @@
-import {Sequelize} from 'sequelize-typescript';
-import { Cita,Abogado,Cliente,Cubiculo,Demanda } from '../data/modelo';
+import { Sequelize } from 'sequelize-typescript';
+import { Cita, Abogado, Cliente, Cubiculo, Caso, Documento } from '../data/modelo';
+const configBd = {
+  bd: process.env.BD_NAME || 'abogados',
+  user: process.env.BD_USER || 'root',
+  pass: process.env.BD_PASS || 'sesamo',
+  host: process.env.BD_HOST || 'localhost',
+  port: Number.parseInt(process.env.BD_PORT || '3306'),
+}
+export const connection = new Sequelize(configBd.bd, configBd.user, configBd.pass, {
+  host: configBd.host,
+  port: configBd.port,
+  dialect: 'mysql'
+})
 
-export const connection = new Sequelize("abogados", 'root', 'sesamo', {
-    host: 'localhost',
-    dialect: 'mysql'
-  })
+connection.addModels([Abogado, Cliente, Cubiculo, Caso, Cita, Documento])
 
-  connection.addModels([Abogado, Cliente, Cubiculo, Demanda, Cita])
-  
