b4bbcc3d5fd0f6287f475dbd15987fa10f433173
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-10-23 23:37:22 -0700

    chore: implementacion de flujos alternativos
    se implementó eliminar cita y mostrar citas programadas

--- cliente/js/Citas.js
+++ cliente/js/Citas.js
@@ -33,6 +33,8 @@ class Citas {
     this.selectCubiculo = document.getElementById("numeroCubiculo");
     this.textAbogado = document.getElementById("motivo");
     this.selectEstado = document.getElementById("estadoCita");
+    this.citasPendientes = document.getElementById("citasPendientes");
+    this.citas = document.querySelector('.citas')
 
     // Agrega manejadores de eventos para los botones y el formulario
     const citasButton = document.getElementById("citasButton");
@@ -69,10 +71,59 @@ class Citas {
     this.mostrarDatosEnSelector('clientes')
     this.mostrarDatosEnSelector('cubiculos')
     crearCitaButton.addEventListener("click", this.crearCita.bind(this));
+    this.selectfecha.addEventListener('change', (e) => {
+      this.cargarCitas(e.target.value)
+    })
   }
 
-  mostrarCitas() {
+  mostrarCitas(citas, fecha) {
+
+    if (!citas) {
+      this.citas.innerHTML = 'Ver citas programadas: 0 citas'
+      this.citasPendientes.innerHTML = `
+      <div class="cita-main-wrapper">
+      No se encontraron citas para la fecha: ${fecha}
+      </div>`
+
+      return
+    }
     // Lógica para mostrar la sección de CITAS, por ejemplo, ocultar otras secciones
+    let citasStr = ''
+    this.citas.innerHTML = 'Ver citas programadas:  ' + citas.length + ' citas'
+    citas.forEach(cita => {
+      citasStr +=
+        `<div class="cita-body">
+
+        <div class="cita-descripcion">${cita.motivo}</div>
+        <div class="cita-hora"> ${cita.fechaInicio.split('T')[1].split('.')[0]}</div>
+        <div class="cita-cubiculo">${cita.cubiculo_idCubiculo}</div>
+        <input class="cita-check" type="checkbox" value="${cita.idCita}" />
+
+       </div>`
+    })
+    this.citasPendientes.innerHTML =
+      `
+      <div class="cita-main-wrapper" id='citas'>
+                     <div class="cita-wrapper">
+                        <div class="cita-body">
+                          <div class="cita-descripcion"><h4>Motivo</h4></div>
+                          <div class="cita-hora"><h4>Hora</div>
+                          <div class="cita-cubiculo"><h4>Cubículo</div>
+                          <div class="cita-check"><h4><p>Seleccionar</p></h4></div>
+                      </div>
+                      
+                      ${citasStr}
+                      </div>
+                  <div class="buttons-main-wrapper">
+                      <div class="buttons-wrapper">
+                          <button class="button-cita" id="editar">modificar</button>
+                          <button class="button-cita" id="eliminar">eliminar</button>
+                      </div>
+                  </div>
+        </div>`
+
+    this.cargarEventosChecks()
+
   }
 
   mostrarExpedientes() {
@@ -108,8 +159,8 @@ class Citas {
       // se convierte a json
       const resJson = await response.json()
       // si al respuesta fue 404 reinicia el selector de casos
-      if (response.status == 404)   this.mostrarCasos()
-      
+      if (response.status == 404) this.mostrarCasos()
+
       // si la respuesta fue status 200 
       if (response.ok) {
         const data = resJson.data
@@ -131,7 +182,7 @@ class Citas {
             break;
         }
 
-      }  else {
+      } else {
         throw new Error(resJson.msg)
       }
 
@@ -240,12 +291,24 @@ class Citas {
     }
   }
   // mostar citas pendientes
-  async cargarCitas() {
+  async cargarCitas(fecha) {
     try {
-      const fechaActual = new Date()
-      const fechaActualHorario = new Date(fechaActual.getTime() - (fechaActual.getTimezoneOffset() * 60000))
-      const parse = fechaActualHorario.toISOString().split('T')
-      const response = await fetch(`${this.url}/citas?fechaActual=${parse[0]}`, {
+      let fechaRequest = ''
+      this.onLoad()
+
+      // si no se a seleccionado fecha, se utiliza la fecha actual y se asigna el valor a la variable fecha request
+      if (!fecha) {
+        const fechaActual = new Date()
+        const fechaActualHorario = new Date(fechaActual.getTime() - (fechaActual.getTimezoneOffset() * 60000))
+        let parse = fechaActualHorario.toISOString().split('T')
+        fechaRequest = parse[0]
+      } else {
+        // en caso contrario se asigna el valor de entrada del datepicker-
+        fechaRequest = fecha
+      }
+
+      // se realiza la peticion http de citas de acuerdo a la fecha especificada en la variable fechaRequest
+      const response = await fetch(`${this.url}/citas?fechaActual=${fechaRequest}`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -255,26 +318,104 @@ class Citas {
       const responseJson = await response.json()
 
       if (response.ok) {
-        /* responseJson.data.forEach(e=>{document.getElementById('citas').innerHTML+=`
-         <div>
-             <span>${e.motivo}</span>
-             <span>${new Date(e.fechaInicio).toUTCString()}</span>
-             <span>${new Date(e.fechaFin).toUTCString()}</span>
-             <span>${e.cubiculo_idCubiculo}</span>
-             </div>
-         `});*/
+        // se filtran solo las programadas
         const pendientes = responseJson.data.filter(e => e.estado == 'programada')
+        // se muestran en pantalla
+        this.mostrarCitas(pendientes)
 
-        console.log('citas programadas', pendientes)
+
       } else {
+        // en caso contrario se muestra mensaje que no hay citas en la fecha 
+        this.mostrarCitas(undefined, fechaRequest)
         throw new Error(responseJson.msg)
       }
 
     } catch (err) {
       console.error(err)
+    } finally {
+      this.onDefault()
     }
   }
+  // eliminar cita
+  async eliminarCita(id) {
+    try {
+      // se realiza una peticion http con el metodo DELETE y el parametro id
+      const response = await fetch(`${this.url}/citas/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
 
+      const resJson = await response.json()
+
+      if (response.ok) {
+
+        alert('Cita eliminada exitosamente')
+        location.reload()
+
+      } else {
+        throw new Error(resJson.msg)
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  cargarEventosChecks() {
+    const checksCitas = document.querySelectorAll('.cita-check')
+    const btnEditar = document.querySelector('#editar')
+    const btnEliminar = document.querySelector('#eliminar')
+    let checked = []
+
+    btnEditar.addEventListener('click', (e) => {
+      if (checked.length < 1) {
+        console.log("debe seleccionar")
+        return
+      }
+
+    })
+    btnEliminar.addEventListener('click', (e) => {
+      if (checked.length < 1) {
+        console.log('debe seleccionar ')
+        return
+      }
+
+      let selection = confirm('Desea eliminar cita?')
+      selection ? this.eliminarCita(checked[0].value) : console.log("")
+    })
+
+
+    checksCitas.forEach(check => {
+      check.addEventListener('change', (e) => {
+        // si no hay un check seleccionado se agrega a la lista
+        if (checked.length < 1) {
+          checked.push({
+            value: e.target.value
+          })
+        } else {
+          //en caso contrario si ya hay un check dentro de la lista 
+          // si es igual se elimina de la lista
+          if (checked[0].value == e.target.value) {
+            checked.pop()
+          }
+          // se establece el valor del check a false 
+          e.target.checked = false
+        }
+
+      })
+
+    })
+  }
+
+  onLoad() {
+    document.body.style.cursor = 'wait'
+  }
+  onDefault() {
+    document.body.style.cursor = 'default'
+  }
 }
 
 
