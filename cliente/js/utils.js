0adb940cf54cd73f162a771b7f007b51e8d90eb5
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-12-05 11:31:19 -0700

    feat: navbar

--- cliente/js/utils.js
+++ cliente/js/utils.js
@@ -63,3 +63,26 @@ async function mostrarCliente(data){
     });
 
 }
+
+function bindNavbar(){
+    document.getElementById('heroes').innerHTML=`
+    <nav>
+    <ul id="button">
+        <li class="rr"><a href="#">R&R ABOGADOS Y ASOCIADOS</a></li>
+        <li><a href="crearCita.html">CITAS</a></li>
+        <li><a href="#">EXPEDIENTES</a></li>
+    <li><a href="casos.html">CASOS</a></li>
+    <li><a href="clientes.html">CLIENTES</a></li>   
+
+    <li><a href="#">REPORTES</a></li>
+        <li>
+            <form action="#" method="post">
+                <input type="text" name="search" placeholder="Búsqueda">
+                <button type="submit">Buscar</button>
+            </form>
+        </li>
+    </ul>
+</nav>`
+}
+
+window.addEventListener("load",bindNavbar());
